2318fa4dcef60ebabdfe8c4063f1d81723e60e79
fieldOfView <aldo@fieldofview.com>
2020-06-19 04:57:45 -0700

    Add readme and metadata

--- README.md
+++ README.md
@@ -1,17 +1,26 @@
-# OctoPrint-ngrok
+# OctoPrint Ngrok Tunnel
 
-**TODO:** Describe what your plugin does.
+This plugin creates a secure tunnel to access OctoPrint remotely. It provides a more secure alternative to using port forwarding to expose your OctoPrint instance to the internet, while giving full and secure access to the OctoPrint web interface and the OctoPrint API. The plugin uses secure tunnels provided by [ngrok](https://ngrok.com). These tunnels are encrypted via SSL and Basic Authentication to prevent snooping.
+
+Each time OctoPrint is (re-)started, the tunnel is created at a new, randomly created subdomain of ngrok.com. If you sign up for a paid account at ngrok.com, you can configure the tunnel to use a fixed subdomain.
+
+You can always find the address of the currently active ngrok tunnel by logging in to the [ngrok dashboard](https://dashboard.ngrok.com/status/tunnels).
 
 ## Setup
 
-Install via the bundled [Plugin Manager](https://github.com/foosel/OctoPrint/wiki/Plugin:-Plugin-Manager)
+Install the plugin via the bundled [Plugin Manager](https://github.com/foosel/OctoPrint/wiki/Plugin:-Plugin-Manager)
 or manually using this URL:
 
     https://github.com/fieldOfView/OctoPrint-ngrok/archive/master.zip
 
-**TODO:** Describe how to install your plugin, if more needs to be done than just installing it via pip or through
-the plugin manager.
+Sign up for a free or paid [ngrok account](https://dashboard.ngrok.com/signup).
+
 
 ## Configuration
 
-**TODO:** Describe your plugin's configuration options (if any).
+The Ngrok Tunnel plugin can be configured via the OctoPrint settings panel.
+
+The ngrok **auth token** is used to authenticate you on the ngrok service and can be found by logging in to the [ngrok dashboard](https://dashboard.ngrok.com/auth/your-authtoken).
+
+The plugin requires you to set up Basic Authentication on the tunnel in addition to the access control already set up on your OctoPrint instance. Any username and password combination can be chosen, but it is recommended to use a different username and password than your OctoPrint credentials, in order not to create a single point of failure.
+
--- octoprint_ngrok/__init__.py
+++ octoprint_ngrok/__init__.py
@@ -1,14 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-### (Don't forget to remove me)
-# This is a basic skeleton for your plugin's __init__.py. You probably want to adjust the class name of your plugin
-# as well as the plugin mixins it's subclassing from. This is really just a basic skeleton to get you started,
-# defining your plugin as a template plugin, settings and asset plugin. Feel free to add or remove mixins
-# as necessary.
-#
-# Take a look at the documentation on what other plugin mixins are available.
-
 import octoprint.plugin
 from octoprint.settings import settings
 
@@ -161,7 +153,10 @@ class NgrokPlugin(octoprint.plugin.SettingsPlugin,
 		)
 		
 		self._logger.info("Opening ngrok tunnel...")
-		options = {"bind_tls":True, "auth":auth_string}
+		options = dict(
+			bind_tls=True,
+			auth=auth_string
+		)
 
 		if self._settings.get(["subdomain"]):
 			options["subdomain"] = self._settings.get(["subdomain"])
@@ -178,17 +173,11 @@ class NgrokPlugin(octoprint.plugin.SettingsPlugin,
 			self._plugin_manager.send_plugin_message(self._identifier, dict(tunnel=self._tunnel_url))
 
 
-# If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
-# ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
-# can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
-__plugin_name__ = "Ngrok Tunnel"
+__license__ = 'GNU Affero General Public License http://www.gnu.org/licenses/agpl.html'
+__copyright__ = "Copyright (C) 2020 Aldo Hoeben / fieldOfView - Released under terms of the AGPLv3 License"
 
-# Starting with OctoPrint 1.4.0 OctoPrint will also support to run under Python 3 in addition to the deprecated
-# Python 2. New plugins should make sure to run under both versions for now. Uncomment one of the following
-# compatibility flags according to what Python versions your plugin supports!
-#__plugin_pythoncompat__ = ">=2.7,<3" # only python 2
-#__plugin_pythoncompat__ = ">=3,<4" # only python 3
-#__plugin_pythoncompat__ = ">=2.7,<4" # python 2 and 3
+__plugin_name__ = "Ngrok Tunnel"
+__plugin_pythoncompat__ = ">=2.7,<4" # python 2 and 3
 
 def __plugin_load__():
 	global __plugin_implementation__
